b8eded8786fdfe81b49d63cc394486a241017326
hanoul <hanoul1124@gmail.com>
2018-11-05 11:50:25 +0900

    settings.production과 database분리

--- app/config/settings/production.py
+++ app/config/settings/production.py
@@ -6,9 +6,6 @@ DEBUG = False
 
 WSGI_APPLICATION = 'config.wsgi.application'
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
+secrets = json.load(open(os.path.join(SECRETS_DIR, 'production.json')))
+
+DATABASES = secrets['DATABASES']
