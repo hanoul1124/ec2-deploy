09b1fbcfdc2fc36ce10dcca7636bd7de752424f2
hanoul <hanoul1124@gmail.com>
2018-11-06 15:33:49 +0900

    Dockerfile.base 오타 수정

--- app/config/settings/production.py
+++ app/config/settings/production.py
@@ -11,7 +11,7 @@ production_set = json.load(open(os.path.join(SECRETS_DIR, 'production.json')))
 DATABASES = production_set['DATABASES']
 
 DEFAULT_FILE_STORAGE = 'config.storages.MediaStorage'
-STATICFILES_STORAGE = 'config.storages.StaticStorage'
+# STATICFILES_STORAGE = 'config.storages.StaticStorage'
 AWS_ACCESS_KEY_ID = production_set['AWS_ACCESS_KEY_ID']
 AWS_SECRET_ACCESS_KEY = production_set['AWS_SECRET_ACCESS_KEY']
 AWS_STORAGE_BUCKET_NAME = production_set['AWS_STORAGE_BUCKET_NAME']
