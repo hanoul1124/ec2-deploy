08069bc8d680e6ba31b42bae50be4b866720e4d8
hanoul <hanoul1124@gmail.com>
2018-11-08 15:18:33 +0900

    custom command 실험2

--- app/members/management/commands/create_dummy_user.py
+++ app/members/management/commands/create_dummy_user.py
@@ -5,4 +5,4 @@ from django.utils.crypto import get_random_string
 class Command(BaseCommand):
     help = "더미 유저를 생성합니다."
     def handle(self, *args, **options):
-        User.objects.create(name=get_random_string(length=10))
+        User.objects.create(username=get_random_string(length=10))
