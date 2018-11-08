c1e6b67fe52bf9d4557d782d94b9e17dd57c55c7
hanoul <hanoul1124@gmail.com>
2018-11-08 15:35:04 +0900

    custom command 실험3

--- app/members/management/commands/create_dummy_user.py
+++ app/members/management/commands/create_dummy_user.py
@@ -1,8 +1,16 @@
+import logging
+
+from django.contrib.auth import get_user_model
 from django.core.management.base import BaseCommand, CommandError
-from members.models import *
+# from members.models import *
+User = get_user_model()
 from django.utils.crypto import get_random_string
+
+logger = logging.getLogger(__name__)
 
 class Command(BaseCommand):
     help = "더미 유저를 생성합니다."
     def handle(self, *args, **options):
-        User.objects.create(username=get_random_string(length=10))
+        username=f'dummy_{get_random_string(length=10)}'
+        User.objects.create_user(username=username)
+        self.stdout.write(f'User {username} created')
